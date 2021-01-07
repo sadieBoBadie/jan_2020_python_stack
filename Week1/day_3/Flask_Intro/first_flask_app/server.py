3b8b66d338799159e6ba12a195484cd67f58d45c
sadieBoBadie <sflick@codingdojo.com>
2021-01-07 08:50:01 -0800

    Flask templates part 2

--- Week1/day_3/Flask_Intro/first_flask_app/server.py
+++ Week1/day_3/Flask_Intro/first_flask_app/server.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def puppies():
     return "<h1 style='color: red'>Puppies are cute!</h1>"
 
+@app.route('/<animal>/<color>')
+@app.route('/<animal>')
 @app.route('/<animal>/<color>/<int:num>')
-def display_animal(animal, color, num):
+def display_animal(animal, color="blue", num=5):
 
     print(f"Animal: {animal}")
     print(f"Color: {color}")
@@ -15,6 +18,7 @@ def display_animal(animal, color, num):
 
 
     return render_template('index.html', animal=animal, color=color, num=num)
+
 
 if __name__=="__main__": 
     app.run(debug=True)
